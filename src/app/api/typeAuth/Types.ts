c9d79f2515e3f0e36ab9d84a724e6914f155ad91
Rebeca <martarebecadecarvalhonerys@gmail.com>
2025-07-14 21:46:33 -0300

    :construction: init auth

---
+++ src/app/api/typeAuth/Types.ts
@@ -0,0 +1,19 @@
+export interface Usuario {
+    id?: string;
+    email: string;
+    papel: "USER" | "ADMIN";
+};
+
+export interface LoginProps {
+    email: string; 
+    senha: string;
+};
+
+export interface ContextProps {
+  usuario: Usuario | null; 
+  isLoading: boolean;
+  login: ({ email, senha }: LoginProps) => Promise<void>;
+  logout: () => void;
+};
+
+
